b786022115bb534c5dc511fe9bba72507e9244aa
Ashwini <ashie.oh@gmail.com>
2016-12-29 10:47:23 +0000

    reduced variables & used times method

--- chap12/ex2.rb
+++ chap12/ex2.rb
@@ -8,19 +8,15 @@ puts "Please enter your birth date (1-31):"
 date = gets.chomp.to_i
 puts
 now = Time.new
-nowyear = now.year
-nowmonth = now.month
-nowdate = now.day
-yDiff = nowyear - year
-mDiff = nowmonth - month
-dDiff = nowdate - date
+yDiff = now.year - year
+mDiff = now.month - month
+dDiff = now.day - date
 if mDiff < 0
 	dDiff = -(dDiff + (mDiff*30))
 	yDiff = yDiff - 1
 	mDiff = 12 + mDiff
 end
 puts "You are %i years %i months and %i days old" %[yDiff, mDiff, dDiff]
-while (yDiff>0)
-	puts '%i:SPANK' %[yDiff]
-	yDiff = yDiff - 1
+yDiff.times do
+	puts 'SPANK'
 end
